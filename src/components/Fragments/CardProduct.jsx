7a4ac6eb886f06472e4c803d5e0ecda4cbd01944
nicholasanayaputra <146567253+nicholasanayaputra@users.noreply.github.com>
2024-01-15 23:39:05 +0700

    refactor

---
+++ src/components/Fragments/CardProduct.jsx
@@ -0,0 +1,42 @@
+const CardProduct = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <div className="bg-[#F4F5F7]">{children}</div>
+    </>
+  );
+};
+
+const Header = (props) => {
+  const { image } = props;
+  return (
+    <img src={image} alt="" className="w-[285px] object-cover h-[301px]" />
+  );
+};
+
+const Body = (props) => {
+  const { name, children } = props;
+  return (
+    <div className="p-4">
+      <h5 className="font-semibold text-xl text-gray-third mb-1">{name}</h5>
+      <p className="font-medium text-sm text-gray-fourth">{children}</p>
+    </div>
+  );
+};
+
+const Footer = (props) => {
+  const { price, discount } = props;
+  return (
+    <div className="px-4 pb-[20px] flex items-center gap-4">
+      <h3 className="font-semibold text-base text-gray-third">{price}</h3>
+      <p className="text-[#B0B0B0] font-normal text-base hidden md:block">
+        {discount}
+      </p>
+    </div>
+  );
+};
+
+CardProduct.Header = Header;
+CardProduct.Body = Body;
+CardProduct.Footer = Footer;
+export default CardProduct;
--- src/pages/home/OurProducts.jsx
+++ src/pages/home/OurProducts.jsx
@@ -1,15 +1,25 @@
-import React from "react";
-import Card from "../../components/Layouts/Card";
-
+import React, { useState } from "react";
+import CardProduct from "../../components/Fragments/CardProduct";
+import { data } from "../../data/data";
 const OurProducts = () => {
+  const [products, setProducts] = useState(data);
   return (
     <div className="max-w-screen-2xl mx-auto container xl:px-28 px-4">
       <h1 className="font-bold text-center text-2xl md:text-3xl text-gray-primary mb-8">
         Our Products
       </h1>
-      <Card />
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {products.map((product) => (
+          <CardProduct key={product.id}>
+            <CardProduct.Header image={product.image} />
+            <CardProduct.Body name={product.name}>
+              {product.desc}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price} discount={product.disc} />
+          </CardProduct>
+        ))}
+      </div>
     </div>
   );
 };
-
 export default OurProducts;
